aca185e14de370ed6709417562e5e80aa59b5778
Praduman03 <praduman03k@gmail.com>
2023-01-03 12:04:45 +0530

    Task done]

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { Route,Routes } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import About from './components/About';
+import Home from './components/Home';
+import Contact from './components/Contact';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <Link style={{color:'white', textDecorationLine:'none', margin:'2.5vh'}} to="/">KALVIUM❤️</Link>
+        <div>
+        <Link style={{color:'white', textDecorationLine:'none'}} className='About' to="/About">About Us</Link>
+        <Link style={{color:'white', textDecorationLine:'none'}} className='About' to="/Contact">Contact</Link>
+        </div>
+      </nav>
+      <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/About' element={<About/>}/>
+      <Route path='/Contact' element={<Contact/>}/>
+      </Routes>
     </div>
   );
 }
